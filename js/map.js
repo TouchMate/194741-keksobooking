0f5211f22c84cf8fa023c0178f31ecb1de03f74f
Alexander Sakaev <sakaev11alexander@gmail.com>
2017-08-22 22:12:14 +0400

    created second function

--- js/map.js
+++ js/map.js
@@ -27,48 +27,49 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * max - min + 1) + min;
 }
 
-for (var i = 1; i <= 8; i++) {
-  var locationX = getRandomNumber(300, 900);
-  var locationY = getRandomNumber(100, 500);
-  offers.push({
-    author: {
-      avatar: 'img/avatars/user0' + i + '.png',
-    },
-    offer: {
-      title: TITLES[getRandomNumber(1, 8)],
-      address: locationX + ', ' + locationY,
-      price:
-      getRandomNumber(1000, 1000000),
-      type:
-      TYPES[getRandomNumber(1, 3)],
-      rooms:
-      getRandomNumber(1, 5),
-      guests:
-      getRandomNumber(1, 10),
-      checkin:
-      TIMINGS[getRandomNumber(1, 3)],
-      checkout:
-      TIMINGS[getRandomNumber(1, 3)],
-      features:
-      FEATURES[getRandomNumber(1, 3)],
-      description:
-      '',
-      photos:
-      []
-    },
-    location: {
-      x: locationX,
-      y: locationY
-    }
-  });
+function addData(arr) {
+  for (var i = 1; i <= 8; i++) {
+    var locationX = getRandomNumber(300, 900);
+    var locationY = getRandomNumber(100, 500);
+    arr.push({
+      author: {
+        avatar: 'img/avatars/user0' + i + '.png',
+      },
+      offer: {
+        title: TITLES[getRandomNumber(1, 8)],
+        address: locationX + ', ' + locationY,
+        price:
+          getRandomNumber(1000, 1000000),
+        type:
+          TYPES[getRandomNumber(1, 3)],
+        rooms:
+          getRandomNumber(1, 5),
+        guests:
+          getRandomNumber(1, 10),
+        checkin:
+          TIMINGS[getRandomNumber(1, 3)],
+        checkout:
+          TIMINGS[getRandomNumber(1, 3)],
+        features:
+          FEATURES[getRandomNumber(1, 3)],
+        description: '',
+        photos: []
+      },
+      location: {
+        x: locationX,
+        y: locationY
+      }
+    });
+  }
 }
+addData(offers);
+console.log(offers);
 
-/*
-for (var i = 0; i < offers.length; i++) {
-var newPinMap = document.innerHTML ='<div class=\'pin\' style=\'left:' + offers[i].location.x + 'px;' +
-  'top:' + offers[i].location.y + 'px>' + '<img src=' + offers[i].author.avatar +' class=\'rounded\' width=\'40\' height=\'40\'></div>';
-};
-*/
+// var newPinMap;
+// for (var i = 0; i < offers.length; i++) {
+//   newPinMap = document.innerHTML ='<div class=\'pin\' style=\'left:' + offers[i].location.x + 'px;' +
+//   'top:' + offers[i].location.y + 'px>' + '<img src=' + offers[i].author.avatar +' class=\'rounded\' width=\'40\' height=\'40\'></div>';
+// };
 
 /*
 var pinOnTheMap = document.querySelector('.tokyo_pin-map');
