d07e0b18debfe68b8999ce6a74520b72835f73c5
Alexander Sakaev <sakaev11alexander@gmail.com>
2017-08-23 00:44:42 +0400

    create element on the map

--- js/map.js
+++ js/map.js
@@ -14,75 +14,69 @@ var FEATURES = ['wifi', 'dishwasher', 'parking',
   'washer', 'elevator', 'conditioner'];
 var offers = [];
 
-
-// function getRandomFeatures() {
-//   var randomLength = Math.floor(Math.random() * FEATURES.length) + 1;
-//   for (var j = 0; j < randomLength; j++) {
-//     console.log(FEATURES[Math.floor(Math.random() * FEATURES.length) + 1]);
-//   }
-//
-// }
-
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * max - min + 1) + min;
 }
-
-function addData(arr) {
-  for (var i = 1; i <= 8; i++) {
-    var locationX = getRandomNumber(300, 900);
-    var locationY = getRandomNumber(100, 500);
-    arr.push({
-      author: {
-        avatar: 'img/avatars/user0' + i + '.png',
-      },
-      offer: {
-        title: TITLES[getRandomNumber(1, 8)],
-        address: locationX + ', ' + locationY,
-        price:
-          getRandomNumber(1000, 1000000),
-        type:
-          TYPES[getRandomNumber(1, 3)],
-        rooms:
-          getRandomNumber(1, 5),
-        guests:
-          getRandomNumber(1, 10),
-        checkin:
-          TIMINGS[getRandomNumber(1, 3)],
-        checkout:
-          TIMINGS[getRandomNumber(1, 3)],
-        features:
-          FEATURES[getRandomNumber(1, 3)],
-        description: '',
-        photos: []
-      },
-      location: {
-        x: locationX,
-        y: locationY
-      }
-    });
+function getRandomAmount(paramArray) {
+  var randomLength = getRandomNumber(1, paramArray.length);
+  var arr = [];
+  for (var j = 0; j < randomLength; j++) {
+    arr.push(paramArray[Math.floor(Math.random() * paramArray.length) + 1]);
   }
+  return arr;
 }
-addData(offers);
-console.log(offers);
 
+for (var i = 1; i <= 8; i++) {
+  var locationX = getRandomNumber(300, 900);
+  var locationY = getRandomNumber(100, 500);
+  offers.push({
+    author: {
+      avatar: 'img/avatars/user0' + i + '.png',
+    },
+    offer: {
+      title: TITLES[getRandomNumber(1, 8)],
+      address: locationX + ', ' + locationY,
+      price:
+        getRandomNumber(1000, 1000000),
+      type:
+        TYPES[getRandomNumber(1, 3)],
+      rooms:
+        getRandomNumber(1, 5),
+      guests:
+        getRandomNumber(1, 10),
+      checkin:
+        TIMINGS[getRandomNumber(1, 3)],
+      checkout:
+        TIMINGS[getRandomNumber(1, 3)],
+      features:
+        FEATURES[getRandomAmount(FEATURES)],
+      description: '',
+      photos: []
+    },
+    location: {
+      x: locationX,
+      y: locationY
+    }
+  });
+}
+//
 // var newPinMap;
-// for (var i = 0; i < offers.length; i++) {
-//   newPinMap = document.innerHTML ='<div class=\'pin\' style=\'left:' + offers[i].location.x + 'px;' +
-//   'top:' + offers[i].location.y + 'px>' + '<img src=' + offers[i].author.avatar +' class=\'rounded\' width=\'40\' height=\'40\'></div>';
+// for (var k = 0; k < offers.length; k++) {
+//   newPinMap = document.innerHTML ='<div class=\'pin\' style=\'left:' + offers[k].location['x'] + 'px;' +
+//   'top:' + offers[k].location['y'] + 'px>' + '<img src=' + offers[k].author.avatar +' class=\'rounded\' width=\'40\' height=\'40\'></div>';
 // };
 
-/*
-var pinOnTheMap = document.querySelector('.tokyo_pin-map');
+var pinOnTheMap = document.querySelector('.tokyo__pin-map');
 var fragment = document.createDocumentFragment();
-debugger;
-for (var i = 0; i < offers.length; i++) {
+for (var q = 0; q < offers.length; q++) {
   var newPinMap = document.createElement('div');
   newPinMap.classList.add('pin');
-  newPinMap.style.left = offers[i].location.x + 'px';
-  newPinMap.style.top = offers[i].location.y + 'px';
-  newPinMap.innerHTML = '<img src=' + offers[i].author.avatar + ' class =\'rounded\' width = \'40\' height=\'40\'>';
+  newPinMap.style.left = offers[q].location['x'] - 24 + 'px';
+  newPinMap.style.top = offers[q].location['y'] - 75 + 'px';
+  newPinMap.innerHTML = '<img src=' + offers[q].author.avatar + ' class =\'rounded\' width = \'40\' height=\'40\'>';
   fragment.appendChild(newPinMap);
 }
 pinOnTheMap.appendChild(fragment);
-*/
+
+
 
